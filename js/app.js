a335aa19452bc8ff3356b9f96d0d57d315924ee9
Malcolm Bird <bird.malcolm@gmail.com>
2014-02-08 18:57:11 +0900

    Finished project
    updated everything

---
+++ js/app.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+	
+	$(".ryu").mouseenter(function(){
+		$(".ryu-still").hide();
+		$(".ryu-ready").show();
+	})
+	.mouseleave(function(){
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+
+  .mousedown(function() {
+    playHadouken();
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $(".hadouken").finish().show()
+    .animate(
+		{'left': '300px'},
+				 500,
+  			function() {
+   			 $(this).hide();
+    		$(this).css('left', '-212px');
+ 		 }
+);
+  })
+  .mouseup(function() {
+      $('.ryu-throwing').hide();
+  $('.ryu-ready').show();// ryu goes back to his ready position
+  });
+
+  $(document).keydown(function(e) {
+	if (e.keyCode == 88) {
+  $(".ryu-still").hide();
+  $("ryu-ready").hide();
+ $(".ryu-cool").show();
+ }
+  });
+
+   $(document).keyup(function(e) {
+	if (e.keyCode == 88) {
+		$(".ryu-still").show();
+ 		$(".ryu-cool").hide();
+ }
+});
+
+  function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+};
+
+
+});
